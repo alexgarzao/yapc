b6bdc3a67999275d5b777837dfeaf7e59b17a48f
Alex S. Garzao <alexgarzao@gmail.com>
2015-11-29 20:40:30 -0200

    Now the server keeps the object data in memory.
    * A map was used to keep the data in memory.
    * With the map, a RWLock was used to avoid concurrency between goroutines.
    * For each objecy is generated a hash key based on your URL.
    * Hit or miss are logged and sent into http headers.
    
    Issue #16.

--- main.go
+++ main.go
@@ -6,11 +6,25 @@ import (
     "log"
     "net"
     "net/http"
-    "io"
     "time"
     "os"
     "os/signal"
     "syscall"
+    "sync"
+    "io/ioutil"
+    "crypto/md5"
+    "encoding/hex"
+)
+
+type CacheMemory struct {
+    sync.RWMutex
+    objectList map[string][]byte
+}
+
+var (
+    cacheMemory = CacheMemory {
+        objectList: make(map[string][]byte),
+    }
 )
 
 const listenAddr = ":8098"
@@ -68,17 +82,57 @@ func handleSignals() {
 
 // Main handler. Receive a request and proxy them to the upstream.
 func handler(w http.ResponseWriter, r *http.Request) {
-    upstreamStatusCode, upstreamResponseLength, elapsedUpstreamGetTime, err := proxyUpstreamRequest(w, r)
+
+    startTime := time.Now()
+
+    hashKey := createHash(r.URL.String())
+
+    // Verify if is a cache hit.
+    isHit, statusCode, responseLength, err := verifyIsHit(hashKey, w)
     if err != nil {
         log.Fatal(err)
     }
 
-    logRequest(r, upstreamStatusCode, upstreamResponseLength, elapsedUpstreamGetTime)
+    if isHit == false {
+        // If is a miss, proxy to the upstream.
+        statusCode, responseLength, err = proxyUpstreamRequest(w, r, hashKey)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+
+    elapsedTime := time.Since(startTime)
+
+    logRequest(r, statusCode, responseLength, elapsedTime, isHit)
+}
+
+func verifyIsHit(hashKey string, w http.ResponseWriter) (exist bool, statusCode int, upstreamResponseLength int, err error) {
+
+    cacheMemory.RLock()
+    body, exist := cacheMemory.objectList[hashKey]
+    cacheMemory.RUnlock()
+
+    if !exist {
+        return
+    }
+
+    // Set Yapc-Cache-State header.
+    w.Header().Set("Yapc-Cache-State", "hit")
+
+    // Using io.Write to dump the data object to the downstream.
+    _, err = w.Write(body)
+    if err != nil {
+        return
+    }
+
+    statusCode = http.StatusOK
+    upstreamResponseLength = len(body)
+
+    return
 }
 
 // Proxy a request to the Upstream.
-func proxyUpstreamRequest(w http.ResponseWriter, r *http.Request) (statusCode int, upstreamResponseLength int64, elapsedTime time.Duration, err error) {
-    startTime := time.Now()
+func proxyUpstreamRequest(w http.ResponseWriter, r *http.Request, hashKey string) (statusCode int, upstreamResponseLength int, err error) {
 
     upstreamResponse, err := http.Get(r.URL.String())
     if err != nil {
@@ -90,14 +144,21 @@ func proxyUpstreamRequest(w http.ResponseWriter, r *http.Request) (statusCode in
     // Set Yapc-Cache-State header.
     w.Header().Set("Yapc-Cache-State", "fetch")
 
-    // Using io.Copy to dump the upstreamResponse to the downstream.
-    upstreamResponseLength, err = io.Copy(w, upstreamResponse.Body)
+    // Assumption: read the whole object and then send to downstream is a good approach only for small objects.
+    // Probably, in another version, the objects will be read in chunks (maybe 4KB).
+    body, err := ioutil.ReadAll(upstreamResponse.Body)
+
+    cacheMemory.Lock()
+    cacheMemory.objectList[hashKey] = body
+    cacheMemory.Unlock()
+
+    _, err = w.Write(body)
     if err != nil {
-        log.Fatal(err)
+        return
     }
 
-    elapsedTime = time.Since(startTime)
     statusCode = upstreamResponse.StatusCode
+    upstreamResponseLength = len(body)
 
     return
 }
@@ -114,17 +175,34 @@ func getIP(r *http.Request) string {
 }
 
 // Log a http request.
-func logRequest(r *http.Request, upstreamStatusCode int, upstreamResponseLength int64, elapsedUpstreamGetTime time.Duration) {
+func logRequest(r *http.Request, upstreamStatusCode int, upstreamResponseLength int, elapsedUpstreamGetTime time.Duration, isHit bool) {
     // Log format: [Timestamp] RemoteAddr "URL" Status BytesSent "HttpUserAgent" UpstreamResponseTime
     // Example   : [2006-01-02T15:04:05Z07:00] 87.19.231.27 "http://www.teste.com.br/xyz" 200 12345 "CURL" 123
+
+    var cacheState string
+    if isHit {
+        cacheState = "HIT"
+    } else {
+        cacheState = "MISS"
+    }
+
     log.Printf(
-        "[%s] %s %q %d %d %q %v",
+        "[%s] %s %s %q %d %d %q %v",
         time.Now().Format(time.RFC3339),
         getIP(r),
+        cacheState,
         r.URL.String(),
         upstreamStatusCode,
         upstreamResponseLength,
         r.UserAgent(),
         elapsedUpstreamGetTime,
     )
+}
+
+// Create a hash key based on the URL.
+func createHash(objectUrl string) (string) {
+    hash := md5.New()
+    hash.Write([]byte (objectUrl))
+    md := hash.Sum(nil)
+    return hex.EncodeToString(md[:])
 }
