1a487ae2fdb188507abe9711f4ff536c19f596e9
Alex S. Garzao <alexgarzao@gmail.com>
2015-11-29 20:35:41 -0200

    New performance tests.
    New objects, more concurrent gets, ...
    
    Issue #16.

--- benchmark/main.py
+++ benchmark/main.py
@@ -7,22 +7,26 @@ PROXY_ADDRESS = "localhost:8098"
 
 # Types of tests.
 test_types = [
-    #   ( "test_description"            , executions_number (-n), concurrent_workers (-c) )
-        ( "1 request, 1 process"        , 1                     , 1                       ),
-        ( "10 requests, 1 process"      , 10                    , 1                       ),
-        ( "10 requests, 10 processes"   , 10                    , 10                      ),
-        ( "100 requests, 10 processes"  , 100                   , 10                      ),
-#        ( "1000 requests, 50 processes" , 1000                  , 50                      ),
-#        ( "1000 requests, 100 processes", 1000                  , 100                     ),
+    #   ( "test_description"             , executions_number (-n), concurrent_workers (-c) )
+        ( "1 request, 1 process"         , 1                     , 1                       ),
+        ( "10 requests, 1 process"       , 10                    , 1                       ),
+        ( "10 requests, 10 processes"    , 10                    , 10                      ),
+        ( "100 requests, 10 processes"   , 100                   , 10                      ),
+        ( "1000 requests, 50 processes"  , 1000                  , 50                      ),
+        ( "1000 requests, 100 processes" , 1000                  , 100                     ),
+#        ( "10000 requests, 10 processes" , 10000                 , 10                      ),
 ]
 
 object_list = [
-    "http://i.imgur.com/t0x900S.jpg",                                              # 118401 bytes (~115 KB).
-    "http://i.imgur.com/BMM9XWU.png",                                              # 52659 bytes (~51 KB).
-#    "http://graphics.stanford.edu/~seander/bithacks.html",                        # Less than 100 KB.
-#    "http://www.learn2crack.com/wp-content/uploads/2015/05/logo31.png",           # Less than 23 KB.
-#    "http://pbs.twimg.com/profile_images/603610759671611392/JRQtMqMR_normal.png", # With less than 2 KB (1655 bytes).
-#    "http://arquivos.oi.com.br/M1.zip",                                           # With 1MB size.
+    "http://i.imgur.com/t0x900S.jpg",                                             # 118401 bytes (~115 KB).
+    "http://i.imgur.com/BMM9XWU.png",                                             # 52659 bytes (~51 KB).
+    "http://graphics.stanford.edu/~seander/bithacks.html",                        # Less than 100 KB.
+    "http://www.learn2crack.com/wp-content/uploads/2015/05/logo31.png",           # Less than 23 KB.
+    "http://pbs.twimg.com/profile_images/603610759671611392/JRQtMqMR_normal.png", # With less than 2 KB (1655 bytes).
+    "http://arquivos.oi.com.br/M1.zip",                                           # With 1MB size.
+    "http://arquivos.oi.com.br/M5.zip",                                           # With 5MB size.
+    "http://arquivos.oi.com.br/M10.zip",                                          # With 10MB size.
+#    "http://arquivos.oi.com.br/M50.zip",                                          # With 50MB size.
 ]
 
 # For each object, execute the test list.
